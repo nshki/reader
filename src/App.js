268c85f883e316b25a4bd55700200c9e52938d99
Nishiki Liu <nshki@users.noreply.github.com>
2018-06-02 22:49:52 -0400

    Add basic RSS feed rendering

--- src/App.js
+++ src/App.js
@@ -1,11 +1,53 @@
 import React, { Component } from 'react';
+import Parser from 'rss-parser';
 
 class App extends Component {
+  state = { inputVal: '', feeds: [], items: [] };
+
+  parser = new Parser();
+  corsProxy = 'https://cors-anywhere.herokuapp.com';
+
+  handleChange = (e) => {
+    this.setState({ inputVal: e.target.value });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const { inputVal, feeds } = this.state;
+    if (!feeds.includes(inputVal)) {
+      this.setState({ feeds: [...feeds, inputVal] });
+    }
+  };
+
+  componentDidUpdate(prevProps, prevState) {
+    const { feeds } = this.state;
+    const newFeeds = feeds.filter((x) => !prevState.feeds.includes(x));
+
+    newFeeds.forEach((feed) => {
+      this.parser.parseURL(`${this.corsProxy}/${feed}`, (err, feed) => {
+        const { items } = this.state;
+        this.setState({ items: items.concat(feed.items) });
+      });
+    });
+  }
+
   render() {
+    const { items } = this.state;
+
     return (
-      <div>
-        Hello Reader!
-      </div>
+      <React.Fragment>
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" onChange={this.handleChange} />
+        </form>
+
+        <ul>
+          {items.map((item, i) =>
+            <li key={`item-${i}`}>
+              <a href={item.link} target="_blank">{item.title}</a>
+            </li>
+          )}
+        </ul>
+      </React.Fragment>
     );
   }
 }
