551c93efb74dfc2945eaee32aece02059fdbe17d
Nishiki Liu <nshki@users.noreply.github.com>
2018-06-03 13:37:56 -0400

    Add input placeholder and display item dates

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Parser from 'rss-parser';
+import moment from 'moment';
 
 class App extends Component {
   state = { inputVal: '', feeds: [], items: [] };
@@ -50,13 +51,18 @@ class App extends Component {
     return (
       <React.Fragment>
         <form onSubmit={this.handleSubmit}>
-          <input type="text" onChange={this.handleChange} />
+          <input
+            type="text"
+            placeholder="Enter RSS URL"
+            onChange={this.handleChange}
+          />
         </form>
 
         <ul>
           {items.map((item, i) =>
             <li key={`item-${i}`}>
-              <a href={item.link} target="_blank">{item.title}</a>
+              <a href={item.link} target="_blank">{item.title}</a><br />
+              {moment(item.pubDate).format('MMM D, YYYY')}
             </li>
           )}
         </ul>
