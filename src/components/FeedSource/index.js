b41205c2a3b8ecc11d3e6cef193ab190504a4ae6
Nishiki Liu <nshki@users.noreply.github.com>
2018-10-21 12:47:16 -0400

    Remove unused ref

--- src/components/FeedSource/index.js
+++ src/components/FeedSource/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
 import { connect } from 'redux-zero/react';
 import actions from '../../actions';
 import { Container, Heading, Action } from './style';
