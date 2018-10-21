7a568e1060fa4e069d4cbcdb359d7114942c47da
Nishiki Liu <nshki@users.noreply.github.com>
2018-10-21 12:38:28 -0400

    Port FeedItems styles

---
+++ src/components/FeedItems/style.js
@@ -0,0 +1,35 @@
+import styled from 'styled-components';
+
+export const Source = styled.div`
+  background-color: #dfb746;
+  margin: 0;
+  padding: 0.5em;
+  text-align: center;
+`;
+
+export const SourceText = styled.p`
+  margin: 0;
+  font-size: 0.8rem;
+  font-weight: 700;
+  color: rgba(0, 0, 0, 0.5);
+`;
+
+export const Container = styled.div`
+  width: 100%;
+  max-width: 600px;
+  margin: 0 auto;
+  padding: 0 20px;
+`;
+
+export const Items = styled.ul`
+  margin: 0;
+  padding: 0;
+  list-style: none;
+`;
+
+export const Item = styled.li`
+  margin: 0;
+  padding: 0;
+`;
+
+export const Text = styled.p``;
